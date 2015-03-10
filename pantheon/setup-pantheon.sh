303b14b10c39c8c60d24ae78c743e21d8968a747
Vincenzo Russo <nemo@neminis.org>
2015-03-10 22:28:34 +0000

    Update provisioning script.

--- pantheon/setup-pantheon.sh
+++ pantheon/setup-pantheon.sh
@@ -1,15 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
-# Install Terminus 0.5.1.
-sudo curl -s https://github.com/pantheon-systems/cli/releases/download/0.5.1/terminus.phar -L -o /usr/local/bin/terminus && sudo chmod +x /usr/local/bin/terminus
+# Install Terminus, latest version.
+TERMINUS_LATEST=$(curl -s -D - https://github.com/pantheon-systems/cli/releases/latest -o /dev/null | grep Location | cut -d "/" -f 8)
+TERMINUS_LATEST=${TERMINUS_LATEST//[^a-zA-Z0-9_.]/}
+sudo curl -s https://github.com/pantheon-systems/cli/releases/download/${TERMINUS_LATEST}/terminus.phar -L -o /usr/local/bin/terminus && sudo chmod +x /usr/local/bin/terminus
 
 # Install Displica.
-cp displica /usr/local/bin/
-chmod 755 /usr/local/bin/displica
+sudo cp /vagrant/pantheon/displica /usr/local/bin/
+sudo chmod 755 /usr/local/bin/displica
 
 # Prepare vhost_alias.
 cd /etc/apache2/mods-enabled/
 sudo ln -s ../mods-available/vhost_alias.load
-sudo cp pantheon.conf /etc/apache2/sites-available/
+sudo cp /vagrant/pantheon/pantheon.conf /etc/apache2/sites-available/
 cd /etc/apache2/sites-enabled/
 sudo ln -s ../sites-available/pantheon.conf
